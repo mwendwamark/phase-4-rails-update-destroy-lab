9f7ac966b3c5ab9c69148c007f371deb9d14d21d
mark mwendwa <mwendwamark310@gmail.com>
2024-01-23 08:20:02 +0300

    completed lab

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,15 +1,15 @@
 class PlantsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   # GET /plants
   def index
     plants = Plant.all
-    render json: plants
+    render json: plants, status: :ok
   end
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
+    plant = find_plant
+    render json: plant, status: :ok
   end
 
   # POST /plants
@@ -18,9 +18,29 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update 
+    plant =  find_plant 
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+  end
+
+  def destroy 
+    plant = find_plant  
+    plant.destroy 
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def render_not_found_response
+    render json: {error: "Plant not found"}, status: :not_found
+  end
+
+  def find_plant 
+    plant = Plant.find(params[:id])
   end
 end
